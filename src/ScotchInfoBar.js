c4806b5f20c96e623e2fe7016b8f97f22d21fecc
Shivanathsai <70058138+Shivanathsai@users.noreply.github.com>
2021-04-02 15:36:38 +0530

    ss

--- src/ScotchInfoBar.js
+++ src/ScotchInfoBar.js
@@ -15,19 +15,7 @@ export default function ScotchInfoBar(props) {
         letterSpacing: "0.5px"
       }}
     >
-      Scotch.io Code Challenges!{" "}
-      <a
-        href=""
-        style={{
-          color: "#40A8F3",
-          textDecoration: "none",
-          display: "inline-block",
-          padding: "0 0 3px",
-          borderBottom: "2px solid #40A8F3"
-        }}
-      >
-        Day {props.seriesNumber} of #10DaysOfReact
-      </a>
+  
     </div>
   );
 }
